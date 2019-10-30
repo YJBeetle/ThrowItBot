c16e38419089999f26af30bcdba02a9e730ecc71
YJBeetle <yjbeetle@gmail.com>
2019-10-31 02:32:11 +0800

    downloadFile ok

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,63 @@
 #include <stdio.h>
 #include <tgbot/tgbot.h>
 
-#include "config.h"
+using namespace std;
+using namespace TgBot;
 
-int main() {
-    TgBot::Bot bot(TOKEN);
-    bot.getEvents().onCommand("start", [&bot](TgBot::Message::Ptr message) {
-        bot.getApi().sendMessage(message->chat->id, "Hi!");
+int main()
+{
+    string token = getenv("TOKEN");
+
+    Bot bot(token);
+
+    bot.getEvents().onCommand("start", [&bot](Message::Ptr message) {
+        bot.getApi().sendMessage(message->chat->id, "Hi! you can say /throw .");
     });
-    bot.getEvents().onAnyMessage([&bot](TgBot::Message::Ptr message) {
+
+    bot.getEvents().onCommand("help", [&bot](Message::Ptr message) {
+        bot.getApi().sendMessage(message->chat->id, "You can say /throw .");
+    });
+
+    bot.getEvents().onCommand("throw", [&bot](Message::Ptr message) {
+        auto photos = bot.getApi().getUserProfilePhotos(message->chat->id);
+        auto fileId = bot.getApi().getFile(photos->photos[0][0]->fileId);
+        auto fileData = bot.getApi().downloadFile(fileId->filePath);
+
+        auto fileNew = make_shared<InputFile>();
+        fileNew->data = fileData;
+        fileNew->mimeType = "image/jpeg";
+        bot.getApi().sendPhoto(message->chat->id, fileNew);
+
+        bot.getApi().sendMessage(message->chat->id, "ok");
+    });
+
+    bot.getEvents().onAnyMessage([&bot](Message::Ptr message) {
         printf("User wrote %s\n", message->text.c_str());
-        if (StringTools::startsWith(message->text, "/start")) {
+        if (
+            StringTools::startsWith(message->text, "/start") ||
+            StringTools::startsWith(message->text, "/help") ||
+            StringTools::startsWith(message->text, "/throw") ||
+            false)
+        {
             return;
         }
-        bot.getApi().sendMessage(message->chat->id, "Your message is: " + message->text);
+        bot.getApi().sendMessage(message->chat->id, "Do you need /help ?");
     });
-    try {
+
+    try
+    {
         printf("Bot username: %s\n", bot.getApi().getMe()->username.c_str());
-        TgBot::TgLongPoll longPoll(bot);
-        while (true) {
+        TgLongPoll longPoll(bot);
+        while (true)
+        {
             printf("Long poll started\n");
             longPoll.start();
         }
-    } catch (TgBot::TgException& e) {
+    }
+    catch (TgException &e)
+    {
         printf("error: %s\n", e.what());
     }
+
     return 0;
 }
