13153b1609cecb5feb229d29978a1cb84ee12478
YJBeetle <yjbeetle@gmail.com>
2019-10-30 23:53:59 +0800

    init

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 # TG_ThrowItBot
 
-TG_ThrowItBot
+Throw It!
+
+## Depend
+
+```
+sudo apt-get install g++ make binutils cmake libssl-dev libboost-system-dev zlib1g-dev
+```
+
+```
+brew install openssl zlib cmake boost@1.59
+```
+
+```
+git clone https://github.com/reo7sp/tgbot-cpp
+cd tgbot-cpp
+cmake .
+make -j4
+sudo make install
+```
+
+## Build
---
+++ src/main.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <tgbot/tgbot.h>
+
+#include "config.h"
+
+int main() {
+    TgBot::Bot bot(TOKEN);
+    bot.getEvents().onCommand("start", [&bot](TgBot::Message::Ptr message) {
+        bot.getApi().sendMessage(message->chat->id, "Hi!");
+    });
+    bot.getEvents().onAnyMessage([&bot](TgBot::Message::Ptr message) {
+        printf("User wrote %s\n", message->text.c_str());
+        if (StringTools::startsWith(message->text, "/start")) {
+            return;
+        }
+        bot.getApi().sendMessage(message->chat->id, "Your message is: " + message->text);
+    });
+    try {
+        printf("Bot username: %s\n", bot.getApi().getMe()->username.c_str());
+        TgBot::TgLongPoll longPoll(bot);
+        while (true) {
+            printf("Long poll started\n");
+            longPoll.start();
+        }
+    } catch (TgBot::TgException& e) {
+        printf("error: %s\n", e.what());
+    }
+    return 0;
+}
